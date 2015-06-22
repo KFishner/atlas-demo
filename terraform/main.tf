9c00fcf886e3e1545825bcca6d8966c819a59273
Kevin Fishner <KFishner@gmail.com>
2015-06-22 18:44:08 -0400

    send tf files

---
+++ terraform/main.tf
@@ -0,0 +1,48 @@
+provider "aws" {
+    access_key = "${var.access_key}"
+    secret_key = "${var.secret_key}"
+    region = "us-east-1"
+}
+
+resource "atlas_artifact" "consul" {
+    name = "KFishner/consul"
+    type = "aws.ami"
+}
+
+resource "atlas_artifact" "masscan" {
+    name = "KFishner/masscan"
+    type = "aws.ami"
+}
+
+resource "aws_instance" "consul" {
+    instance_type = "t2.micro"
+    ami = "${atlas_artifact.consul.metadata_full.region-us-east-1}"
+    key_name = "kfishner"
+
+    count = "3"
+}
+
+resource "aws_instance" "masscan" {
+    instance_type = "t2.micro"
+    ami = "${atlas_artifact.masscan.metadata_full.region-us-east-1}"
+    key_name = "kfishner"
+
+    count = "3"
+}
+
+resource "aws_elb" "web" {
+    name = "terraform-demo-elb"
+
+    # The same availability zone as our instances
+    availability_zones = ["${aws_instance.masscan.*.availability_zone}"]
+
+    listener {
+        instance_port = 80
+        instance_protocol = "http"
+        lb_port = 80
+        lb_protocol = "http"
+    }
+
+    # The instances are registered automatically
+    instances = ["${aws_instance.masscan.*.id}"]
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "access_key" {
+	description = "AWS access key"
+}
+
+variable "secret_key" {
+	description = "AWS secret key"
+}
+
+variable "masscan_count" {
+	description = "count of masscan instances"
+}
+
+variable "consul_count" {
+	description = "count of consul instances"
+}
